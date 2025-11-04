500d51acbb2d6665e77cb80699aa5a29cf49fe90
João Marcos Cavalcanti <joaomarcosnog10@gmail.com>
2025-11-03 23:04:38 -0300

    feat: Configuração inicial do projeto

---
+++ src/br/edu/icev/aed/forense/AnaliseForense.java
@@ -0,0 +1,36 @@
+package br.edu.icev.aed.forense;
+import java.io.*;
+import java.util.*;
+import br.edu.icev.aed.forense.AnaliseForenseAvancada;
+import br.edu.icev.aed.forense.Alerta;
+public class AnaliseForense implements AnaliseForenseAvancada {
+
+    public AnaliseForense() {
+    }
+
+    @Override
+        public Set<String> encontrarSessoesInvalidas(String arquivo) throws IOException {
+            // Implementar usando Map<String, Stack<String>>
+        }
+
+        @Override
+        public List<String> reconstruirLinhaTempo(String arquivo, String sessionId) throws IOException {
+            // Implementar usando Queue<String>
+        }
+
+        @Override
+        public List<Alerta> priorizarAlertas(String arquivo, int n) throws IOException {
+            // Implementar usando PriorityQueue<Alerta>
+        }
+
+        @Override
+        public Map<Long, Long> encontrarPicosTransferencia(String arquivo) throws IOException {
+            // Implementar usando Stack (Next Greater Element)
+        }
+
+        @Override
+        public Optional<List<String>> rastrearContaminacao(String arquivo, String origem, String destino) throws IOException {
+            // Implementar usando BFS em grafo
+        }
+    }
+
